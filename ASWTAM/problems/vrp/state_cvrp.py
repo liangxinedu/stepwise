7729c63997e298a447b29d1d5e9da083b7cf02d6
XIN LIANG <liangxinedu>
2021-09-11 21:04:25 +0800

    update for torch 1.4.0

--- ASWTAM/problems/vrp/state_cvrp.py
+++ ASWTAM/problems/vrp/state_cvrp.py
@@ -143,16 +143,14 @@ class StateCVRP(NamedTuple):
         else:
             visited_loc = mask_long2bool(self.visited_, n=self.demand.size(-1))
 
+        # For demand steps_dim is inserted by indexing with id, for used_capacity insert node dim for broadcasting
+        exceeds_cap = (self.demand[self.ids, :] + self.used_capacity[:, :, None] > self.VEHICLE_CAPACITY)
         # Nodes that cannot be visited are already visited or too much demand to be served now
-        mask_loc = (
-            visited_loc |
-            # For demand steps_dim is inserted by indexing with id, for used_capacity insert node dim for broadcasting
-            (self.demand[self.ids, :] + self.used_capacity[:, :, None] > self.VEHICLE_CAPACITY)
-        )
+        mask_loc = visited_loc.to(exceeds_cap.dtype) | exceeds_cap
 
         # Cannot visit the depot if just visited and still unserved nodes
         mask_depot = (self.prev_a == 0) & ((mask_loc == 0).int().sum(-1) > 0)
-        return torch.cat((mask_depot[:, :, None] > 0, mask_loc > 0), -1)
+        return torch.cat((mask_depot[:, :, None], mask_loc), -1)
 
     def construct_solutions(self, actions):
         return actions
